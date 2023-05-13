9e0645f94548a837515599ed70d0581492f6e954
Hoang Vi <111053566+Hoangvi-forwork@users.noreply.github.com>
2023-05-13 08:39:48 +0700

    Prepare before swich to Dev

--- lib/main.dart
+++ lib/main.dart
@@ -13,6 +13,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      title: "Medical App",
       theme: ThemeData(
         backgroundColor: AppColors.backgroundColor,
         fontFamily: GoogleFonts.poppins().toString(),
