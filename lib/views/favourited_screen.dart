6830e381d580de7493441196a4c3d1699f3073df
Hoang Vi <111053566+Hoangvi-forwork@users.noreply.github.com>
2023-06-22 15:59:46 +0700

    Rebuild Search Screen

--- lib/views/favourited_screen.dart
+++ lib/views/favourited_screen.dart
@@ -14,13 +14,26 @@ class FavouritedScreen extends StatelessWidget {
     final GlobalKey<ScaffoldState> _scaffoldKey = GlobalKey<ScaffoldState>();
     return Scaffold(
       body: Center(
-        child: Container(
-          width: 160,
-          height: 160,
-          child: SvgPicture.string(
-            '<svg width="200" height="200" viewBox="0 0 200 200" fill="none" xmlns="http://www.w3.org/2000/svg"> <g clip-path="url(#clip0_119_294)"> <path d="M99.6399 68.482C99.6399 -65.4274 141.001 28.3603 107.521 86.3525C141.001 28.3603 242.909 17.2836 126.935 84.2437C242.909 17.2836 182.373 99.9946 115.402 99.9946C182.351 99.9946 242.909 182.706 126.935 115.756C242.909 182.706 141.001 171.629 107.521 113.637C141.001 171.629 99.6399 265.427 99.6399 131.507C99.6399 265.427 58.3333 171.629 91.759 113.637C58.2898 171.629 -43.6179 182.706 72.3558 115.756C-43.6179 182.706 16.918 99.9946 83.889 99.9946C16.9288 99.9946 -43.6179 17.2836 72.3558 84.2437C-43.6179 17.2836 58.2898 28.3603 91.759 86.3525C58.2898 28.3603 99.6399 -65.4274 99.6399 68.482Z" fill="url(#paint0_linear_119_294)"/> </g> <defs> <linearGradient id="paint0_linear_119_294" x1="32.4775" y1="18.998" x2="155.898" y2="165.34" gradientUnits="userSpaceOnUse"> <stop stop-color="#FFD9A0"/> <stop offset="1" stop-color="#FFF5F1"/> </linearGradient> <clipPath id="clip0_119_294"> <rect width="200" height="200" fill="white"/> </clipPath> </defs> </svg>',
-            color: Color.fromARGB(255, 25, 6, 102),
-          ),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Container(
+              width: 160,
+              height: 160,
+              margin: const EdgeInsets.only(bottom: 24),
+              child: Image.asset(
+                'assets/icons/health-check.png',
+                fit: BoxFit.cover,
+              ),
+            ),
+            const Text(
+              "Danh sách bạn quan tâm sẽ hiển thị ở đây",
+              style: TextStyle(
+                fontSize: 12,
+                fontStyle: FontStyle.italic,
+              ),
+            )
+          ],
         ),
       ),
     );
--- lib/views/search_sreen.dart
+++ lib/views/search_sreen.dart
@@ -1,29 +1,111 @@
-import 'package:flutter/material.dart';
-import 'package:flutter_svg/flutter_svg.dart';
+// ignore_for_file: unused_local_variable
 
-class SearchScreen extends StatelessWidget {
+import 'package:flutter/material.dart';
+import 'package:medical_app/widgets/colors.dart';
+
+class SearchScreen extends StatefulWidget {
   const SearchScreen({Key? key}) : super(key: key);
 
   @override
+  State<SearchScreen> createState() => _SearchScreenState();
+}
+
+class _SearchScreenState extends State<SearchScreen> {
+  // List<String> selectedDepartment = [
+  //   'Khoa Tim mạch Can thiệp',
+  //   'Khoa Tim mạch tổng quát',
+  //   'Khoa Nhịp tim học',
+  //   'Hồi sức tim mạch',
+  //   'Khoa Phẫu thuật Tim - Lồng ngực mạch máu',
+  //   'Khoa Nội Tiêu hóa',
+  //   'Khoa Nội Thần kinh tổng quát',
+  //   'Khoa Ngoại Thần kinh',
+  //   'Khoa Nội tiết',
+  //   'Khoa Bệnh lý mạch máu não',
+  //   'Khoa Bệnh Nhiệt đới',
+  //   'Khoa Cơ xương khớp',
+  //   'Khoa Hô hấp - Hồi sức tim mạch',
+  //   'Khoa Ngoại Niệu - Ghép thận',
+  //   'Khoa Nội Thận - Miễn dịch ghép',
+  //   'Khoa Cấp cứu Tổng hợp',
+  //   'Khoa Hồi sức tích cực - Chống độc',
+  //   'Khoa Gây mê - Hồi sức Ngoại',
+  //   'Khoa Ngoại tổng quát',
+  //   'Khoa Ngoại Chấn thương chỉnh hình',
+  //   'Khoa Tai mũi họng',
+  //   'Khoa Răng Hàm Mặt - Mắt',
+  //   'Khoa Y học cổ truyền - Phục hồi chức năng',
+  //   'Khoa Điều Trị Theo Yêu Cầu - Y Học Thể Thao',
+  //   'Khoa Khám bệnh',
+  //   'Khoa Khám và Điều trị theo yêu cầu',
+  //   'Khoa Xét nghiệm',
+  //   'Khoa Chẩn đoán hình ảnh',
+  //   'Khoa Giải phẫu bệnh',
+  //   'Đơn vị Nội soi',
+  //   'Khoa Dinh dưỡng',
+  //   'Khoa Kiểm soát nhiễm khuẩn',
+  //   'Khoa Dược',
+  //   'Khoa Ung bướu và Y học hạt nhân',
+  //   'Nhà thuốc',
+  //   'Phòng Tổ chức Cán bộ',
+  //   'Phòng Kế hoạch Tổng hợp',
+  //   'Phòng Điều dưỡng',
+  //   'Phòng Chỉ đạo tuyến',
+  //   'Phòng Tài chính Kế toán',
+  //   'Phòng Hành chính Quản trị',
+  //   'Phòng Vật tư - Thiết bị y tế',
+  //   'Phòng Công nghệ thông tin',
+  //   'Phòng Quản lý chất lượng',
+  //   'Phòng Công tác xã hội'
+  // ];
+
+  @override
   Widget build(BuildContext context) {
-    final GlobalKey<ScaffoldState> _scaffoldKey = GlobalKey<ScaffoldState>();
+    final GlobalKey<ScaffoldState> scaffoldKey = GlobalKey<ScaffoldState>();
     return Scaffold(
-      backgroundColor: Colors.transparent,
-      body: Stack(
-        children: [
-          // Use SvgPicture as a background
-          Positioned.fill(
-            child: SvgPicture.asset(
-              'assets/images/searchBackground.svg',
-              color: Colors.transparent,
-              fit: BoxFit.cover,
+      backgroundColor: Colors.white,
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.end,
+          mainAxisAlignment: MainAxisAlignment.end,
+          children: [
+            Row(
+              children: [
+                Expanded(
+                  flex: 1,
+                  child: Container(
+                    padding: const EdgeInsets.all(6),
+                    child: Center(
+                      child: TextFormField(
+                        style: const TextStyle(color: Colors.grey),
+                        cursorColor: Colors.grey,
+                        decoration: InputDecoration(
+                          hintText: "Nhập tìm kiếm",
+                          prefixIcon: const Icon(Icons.search),
+                          border: OutlineInputBorder(
+                            borderSide: const BorderSide(
+                              color: Colors.black,
+                              width: 1,
+                            ),
+                            borderRadius: BorderRadius.circular(24),
+                          ),
+                          focusedBorder: OutlineInputBorder(
+                            borderSide: const BorderSide(
+                              color: AppColors.primaryColor,
+                              width: 1,
+                            ),
+                            borderRadius: BorderRadius.circular(24),
+                          ),
+                        ),
+                      ),
+                    ),
+                  ),
+                ),
+              ],
             ),
-          ),
-          // Use Center to position your text
-          const Center(
-            child: Text("Tìm kiếm"),
-          ),
-        ],
+          ],
+        ),
       ),
     );
   }
