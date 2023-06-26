349f791c48691c3e5306c92d980b51edefdf9cfb
HoangVu-startwork <hoangvu.startwork@gmail.com>
2023-06-27 06:50:21 +0700

    edit log in

--- backend/index.js
+++ backend/index.js
@@ -31,46 +31,64 @@ app.use(session({
 }));
 app.use(bodyParser.urlencoded({ extended: true }));
 
-// app.get('/sanpham', function (req, res) {
-//      db.query("SELECT `name` FROM users", function (error, results, fields) {
-//          if (error) throw error;
-//          return res.send({ error: false, data: results, message: 'users list.' });
-//      });
-//  });
-
-// app.post('/uploads', MultipartyMiddleware, (req, res) => {
-//     console.log(req.files.upload)
-// })
-
-app.get("/Danhsachbenh", (req, res) => {
+app.get("/danhsachbenh", (req, res) => {
     db.query("SELECT * FROM tbl_benh", (err, result) => {
         if (err) {
-            res.status(422).json("Kong")
+            res.status(422).json("không thực hiện được")
         }else{
             res.status(201).json(result);
         }
     })
 });
 
-app.get("/Khoabenh", (req, res) => {
+app.get("/khoabenh", (req, res) => {
     db.query("SELECT * FROM tbl_khoabenh", (err, result) => {
         if (err) {
-            res.status(422).json("Kong")
+            res.status(422).json("không thực hiện được")
         }else{
             res.status(201).json(result);
         }
     })
 });
 
-app.get("/Loaibenh", (req, res) => {
+app.get("/loaibenh", (req, res) => {
     db.query("SELECT * FROM tbl_loaibenh", (err, result) => {
         if (err) {
-            res.status(422).json("Kong")
+            res.status(422).json("không thực hiện được")
         }else{
             res.status(201).json(result);
         }
     })
 });
+
+app.post("/login", (req, res) => {
+    const email = req.body.email;
+    const password = req.body.password;
+
+    db.query(
+        "SELECT * FROM tbl_taikhoan WHERE email = ?",
+        email,
+        (err, results) => {
+            if (err) {
+                res.send({ err: err });
+            }
+            if (results.length > 0) {
+                    if (results[0].password === password) {
+                        req.session.user = results;
+                        console.log(req.session.user);
+                        res.send({ message: "Tài khoản mật chính sác" });
+                        console.log({ message: email })
+                    } else {
+                        res.send({ message: "Tài khoản mật không chính sác" });
+                        console.log({ message: password })
+                        console.log({ message: email })
+                    }
+            } else {
+                res.send({ message: "Tài khoản không có" });
+            }
+        }
+    )
+})
 
 app.listen(port, () => {
     console.log("Server is");
