3591efb1573763f3ea805509e2074ebcec37930e
HoangVu-startwork <hoangvu.startwork@gmail.com>
2023-06-27 07:22:56 +0700

    edit lại code

--- backend/index.js
+++ backend/index.js
@@ -20,7 +20,6 @@ app.use(cors({
     credentials: true
 }));
 app.use(express.json());
-app.use(bodyParser.json());
 app.use(session({
     key: "userId",
     secret: "subscribe",
@@ -32,67 +31,35 @@ app.use(session({
 }));
 app.use(bodyParser.urlencoded({ extended: true }));
 
-// app.get('/sanpham', function (req, res) {
-//      db.query("SELECT `name` FROM users", function (error, results, fields) {
-//          if (error) throw error;
-//          return res.send({ error: false, data: results, message: 'users list.' });
-//      });
-//  });
-
-// app.post('/uploads', MultipartyMiddleware, (req, res) => {
-//     console.log(req.files.upload)
-// })
-
-app.get("/Danhsachbenh", (req, res) => {
+app.get("/danhsachbenh", (req, res) => {
     db.query("SELECT * FROM tbl_benh", (err, result) => {
         if (err) {
-            res.status(422).json("Kong")
-        } else {
+            res.status(422).json("không thực hiện được")
+        }else{
             res.status(201).json(result);
         }
     })
 });
 
-app.get("/Khoabenh", (req, res) => {
+app.get("/khoabenh", (req, res) => {
     db.query("SELECT * FROM tbl_khoabenh", (err, result) => {
         if (err) {
-            res.status(422).json("Kong")
-        } else {
+            res.status(422).json("không thực hiện được")
+        }else{
             res.status(201).json(result);
         }
     })
 });
 
-app.get("/Loaibenh", (req, res) => {
+app.get("/loaibenh", (req, res) => {
     db.query("SELECT * FROM tbl_loaibenh", (err, result) => {
         if (err) {
-            res.status(422).json("Kong")
-        } else {
+            res.status(422).json("không thực hiện được")
+        }else{
             res.status(201).json(result);
         }
     })
 });
-
-// app.post('/login', (req, res) => {
-//     const { email, password } = req.body;
-
-//     connection.query('SELECT * FROM tbl_taikhoan WHERE email = ?', [email], (error, results) => {
-//       if (error) throw error;
-
-//       if (results.length > 0) {
-//         if (results[0].password === password) {
-//           res.status(200).json({ message: 'Đăng nhập thành công' });
-//           console.log('Sai mật khẩu');
-//         } else {
-//           res.status(401).json({ message: 'Sai mật khẩu' });
-//           console.log('Sai mật khẩu');
-//         }
-//       } else {
-//         res.status(404).json({ message: 'Người dùng không tồn tại' });
-//         console.log('Sai mật khẩu');
-//       }
-//     });
-//   });
 
 app.post("/login", (req, res) => {
     const email = req.body.email;
@@ -122,15 +89,6 @@ app.post("/login", (req, res) => {
         }
     )
 })
-
-app.get("/login", (req, res) => {
-    if (req.session.user) {
-      res.send({ loggedIn: true, user: req.session.user });
-    } else {
-      res.send({ loggedIn: false });
-    }
-});
-
 
 app.listen(port, () => {
     console.log("Server is");
