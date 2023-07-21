3953233d9e52c0394aab0e123e27299aefa0718f
HoangVu-sartword <121810650+HoangVu-startwork@users.noreply.github.com>
2023-07-21 17:32:41 +0700

    Update benh.js

--- backend/app/khoabenh/benh.js
+++ backend/app/khoabenh/benh.js
@@ -14,62 +14,6 @@ app.get("/api/danhsachbenh", (req, res) => {
   });
 });
 
-// app.get("/api/khoabenh/:idKhoabenh", (req, res) => {
-//   db.query(
-//     "SELECT * FROM tbl_khoabenh WHERE idKhoabenh = ?",
-//     [req.params.idKhoabenh],
-//     (err, result) => {
-//       if (err) {
-//         res.status(422).json("Không thực hiện được");
-//       } else {
-//         res.status(201).json(result);
-//       }
-//     }
-//   );
-// });
-
-// app.get("/api/loaibenh/", (req, res) => {
-//   const sql = "SELECT * FROM tbl_loaibenh";
-//   db.query(sql, (err, result) => {
-//     if (err) {
-//       console.log(err);
-//     }
-//     res.send(result);
-//   });
-// });
-
-// app.get("/api/khoabenh", (req, res) => {
-//   db.query("SELECT * FROM tbl_khoabenh WHERE id = ?", (err, result) => {
-//     if (err) {
-//       res.status(422).json("Không thực hiện được");
-//     } else {
-//       res.status(201).json(result);
-//     }
-//   });
-// });
-
-// app.get("/api/loaibenh", (req, res) => {
-//   db.query("SELECT * FROM tbl_loaibenh", (err, result) => {
-//     if (err) {
-//       res.status(422).json("không thực hiện được");
-//     } else {
-//       res.status(201).json(result);
-//     }
-//   });
-// });
-
-// app.get("/api/loaibenh/:idLoaibenh", (req, res) => {
-//   const { idLoaibenh } = req.params;
-//   const sql = "SELECT * FROM tbl_loaibenh WHERE idLoaibenh = ?";
-//   db.query(sql, [idLoaibenh], (err, result) => {
-//     if (err) {
-//       console.log(err);
-//       res.status(422).json("Không thực hiện được");
-//     } else {
-//       res.send(result);
-//     }
-//   });
-// });
 
 app.get("/api/benh/:tenbenh", (req, res) => {
   const { tenbenh } = req.params;
