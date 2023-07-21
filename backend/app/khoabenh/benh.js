d53436943796d0b9cef3fab62b730cc0c950a99d
HoangVu-sartword <121810650+HoangVu-startwork@users.noreply.github.com>
2023-07-21 16:33:50 +0700

    edit dev

--- backend/app/khoabenh/benh.js
+++ backend/app/khoabenh/benh.js
@@ -14,48 +14,6 @@ app.get("/api/danhsachbenh", (req, res) => {
   });
 });
 
-app.get("/api/khoabenh/:idKhoabenh", (req, res) => {
-
-    db.query("SELECT * FROM tbl_khoabenh WHERE idKhoabenh = ''", (err, result) => {
-        if (err) {
-            res.status(422).json("Không thực hiện được");
-        } else {
-            res.status(201).json(result);
-        }
-    })
-})
-
-  db.query("SELECT * FROM tbl_khoabenh WHERE id = ''", (err, result) => {
-    if (err) {
-      res.status(422).json("Không thực hiện được");
-    } else {
-      res.status(201).json(result);
-    }
-  });
-});
-
-
-app.get(`/api/khoabenh/:idKhoabenh`, (req, res) => {
-  const { idKhoabenh } = req.params;
-  const sql = "SELECT * FROM tbl_khoabenh WHERE idKhoabenh = ?";
-  db.query(sql, idKhoabenh, (err, result) => {
-    if (err) {
-      console.log(err);
-    }
-    res.send(result);
-  });
-});
-
-app.get("/api/loaibenh", (req, res) => {
-  db.query("SELECT * FROM tbl_loaibenh", (err, result) => {
-    if (err) {
-      res.status(422).json("không thực hiện được");
-    } else {
-      res.status(201).json(result);
-      // print(result.body);
-    }
-  });
-});
 
 app.get("/api/loaibenh/:idLoaibenh", (req, res) => {
 
@@ -69,27 +27,6 @@ app.get("/api/loaibenh/:idLoaibenh", (req, res) => {
         });
 })
 
-app.get("/api/benh/:tenbenh", (req, res) => {
-    const { tenbenh } = req.params;
-    const sql = "SELECT * FROM tbl_benh WHERE tenbenh = ?";
-    db.query(sql, tenbenh, (err, result) => {
-        if (err) {
-            console.log(err);
-        }
-        res.send(result);
-    });
-});
-
-
-  const { idLoaibenh } = req.params;
-  const sql = "SELECT * FROM tbl_loaibenh WHERE idLoaibenh = ?";
-  db.query(sql, idLoaibenh, (err, result) => {
-    if (err) {
-      console.log(err);
-    }
-    res.send(result);
-  });
-});
 
 app.get("/api/loaibenh/:idLoaibenh", (req, res) => {});
 
